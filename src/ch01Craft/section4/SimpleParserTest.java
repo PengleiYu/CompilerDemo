5bb19205cc7ccbab86d12fae55b1fd1fdcaed93c
yupenglei <yupengniaosanshi@gmail.com>
2020-01-05 00:01:36 +0800

    语法解析：一个完整的简单语法解析器

--- src/ch01Craft/section4/SimpleParserTest.java
+++ src/ch01Craft/section4/SimpleParserTest.java
@@ -8,14 +8,14 @@ import org.junit.Test;
  */
 public class SimpleParserTest {
     @Test
-    public void parse() throws Exception {
+    public void evaluate() throws Exception {
         String[] scripts = {
-                "1+2",
-                "1+2+3+4",
-                "2*3",
-                "2*3*4*5",
-                "2*3/4",
-                "4+2-3+3*3/4*5",
+                "1+2;",
+                "1+2+3+4;",
+                "2*3;",
+                "2*3*4*5;",
+                "2*3/4;",
+                "4+2-3+3*3/4*5;",
         };
         SimpleParser parser = new SimpleParser();
 
@@ -25,7 +25,17 @@ public class SimpleParserTest {
             parser.evaluate(node, "");
             System.out.println("============================================================");
         }
-
     }
 
+    @Test
+    public void parse() throws Exception {
+        String[] scripts = {
+                "int a = 30; int b; b= 40; a = b; b= 1+2+3; 2*3*4;",
+        };
+        SimpleParser parser = new SimpleParser();
+        for (String script : scripts) {
+            ASTNode astNode = parser.parse(script);
+            parser.dumpAST(astNode, "");
+        }
+    }
 }

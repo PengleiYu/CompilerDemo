37c0cb2134b9b9bde869649fb8cda0757230a487
yupenglei <yupengniaosanshi@gmail.com>
2020-01-04 12:36:15 +0800

    语法分析：实现简单的四则运算计算器

--- src/ch01Craft/section03/SimpleCalculator.java
+++ src/ch01Craft/section03/SimpleCalculator.java
@@ -8,11 +8,15 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by JJBOOM on 2020/01/04.
+ * 实现一个简单的简单的计算器
+ * <p>
+ * 目前缺陷：默认右结合，导致一些场景下计算错误
  */
 public class SimpleCalculator {
 
-
+    /**
+     * 使用正则文法
+     */
     public SimpleASTNode intDeclare(TokenReader reader) throws Exception {
         SimpleASTNode node = null;
         Token token = reader.peek();
@@ -46,6 +50,23 @@ public class SimpleCalculator {
         return node;
     }
 
+    public int evaluate(String script) throws Exception {
+        ASTNode tree = parse(script);
+        dumpAST(tree, "");
+        return evaluate(tree, "");
+    }
+
+    public void dumpAST(ASTNode node, String indent) {
+        System.out.println(indent + node.getType() + " " + node.getText());
+        for (ASTNode astNode : node.getChildren()) {
+            dumpAST(astNode, indent + "\t");
+        }
+    }
+
+    /**
+     * 解析加法表达式
+     * 为回避左递归导致了右结合问题
+     */
     private SimpleASTNode additive(TokenReader reader) throws Exception {
         SimpleASTNode node = null;
         SimpleASTNode child1 = multiplicative(reader);
@@ -53,11 +74,11 @@ public class SimpleCalculator {
             Token token = reader.peek();
             if (token != null && (token.getType() == TokenType.Minus || token.getType() == TokenType.Plus)) {
                 token = reader.read();
-                ASTNode child2 = multiplicative(reader);
+                ASTNode child2 = additive(reader);
                 if (child2 == null) {
                     throw new Exception("非法的加法表达式，需要右半部分");
                 } else {
-                    node = new SimpleASTNode(ASTNodeType.Multiplicative, token.getText());
+                    node = new SimpleASTNode(ASTNodeType.Additive, token.getText());
                     node.addChild(child1);
                     node.addChild(child2);
                 }
@@ -68,6 +89,10 @@ public class SimpleCalculator {
         return node;
     }
 
+    /**
+     * 解析乘法表达式
+     * 为回避左递归导致了右结合问题
+     */
     private SimpleASTNode multiplicative(TokenReader reader) throws Exception {
         SimpleASTNode node = null;
         SimpleASTNode child1 = primary(reader);
@@ -76,7 +101,7 @@ public class SimpleCalculator {
             if (token != null &&
                     (token.getType() == TokenType.Star || token.getType() == TokenType.Slash)) {
                 token = reader.read();
-                SimpleASTNode child2 = primary(reader);
+                SimpleASTNode child2 = multiplicative(reader);
                 if (child2 == null) {
                     throw new Exception("非法乘法表达式，需要右半部分");
                 } else {
@@ -91,6 +116,10 @@ public class SimpleCalculator {
         return node;
     }
 
+    /**
+     * 解析原始类型数据
+     * 包括int字面值和括号表达式
+     */
     private SimpleASTNode primary(TokenReader reader) throws Exception {
         SimpleASTNode node = null;
         Token token = reader.peek();
@@ -119,32 +148,68 @@ public class SimpleCalculator {
         return node;
     }
 
-
-    public void evaluate(String script) {
-        ASTNode tree = parse(script);
-        dumpAST(tree, "");
-        evaluate(tree, "");
-    }
-
-    private void evaluate(ASTNode tree, String s) {
-
-    }
-
-    public void dumpAST(ASTNode node, String indent) {
-        System.out.println(indent + node.getType() + " " + node.getText());
-        for (ASTNode astNode : node.getChildren()) {
-            dumpAST(astNode, indent + "\t");
-        }
-    }
-
-    private ASTNode parse(String script) {
+    private ASTNode parse(String script) throws Exception {
         SimpleLexer lexer = new SimpleLexer();
         TokenReader tokens = lexer.tokenize(script);
-        return prog(tokens);
+        return program(tokens);
     }
 
-    private ASTNode prog(TokenReader tokens) {
-        return null;
+    /**
+     * @param node   AST根节点
+     * @param indent 格式化间隔
+     * @return 根据抽象语法树计算出的结果
+     */
+    private int evaluate(ASTNode node, String indent) throws Exception {
+        int result;
+        System.out.println(indent + "Calculating " + node.getType());
+        switch (node.getType()) {
+            case Programm:
+                result = evaluate(node.getChildren().get(0), indent);
+                break;
+            case Multiplicative:
+                ASTNode left = node.getChildren().get(0);
+                ASTNode right = node.getChildren().get(1);
+                int val1 = evaluate(left, indent + "\t");
+                int val2 = evaluate(right, indent + "\t");
+                if ("*".equals(node.getText())) {
+                    result = val1 * val2;
+                } else {
+                    result = val1 / val2;
+                }
+                break;
+            case Additive:
+                left = node.getChildren().get(0);
+                right = node.getChildren().get(1);
+                val1 = evaluate(left, indent + "\t");
+                val2 = evaluate(right, indent + "\t");
+                if ("+".equals(node.getText())) {
+                    result = val1 + val2;
+                } else {
+                    result = val1 - val2;
+                }
+                break;
+            case IntLiteral:
+                result = Integer.parseInt(node.getText());
+                break;
+            default:
+                throw new Exception("非法类型");
+        }
+        System.out.println(indent + "Result: " + result);
+        return result;
+    }
+
+    /**
+     * @param reader 词法序列
+     * @return 抽象语法树
+     */
+    private ASTNode program(TokenReader reader) throws Exception {
+        //        SimpleASTNode node = new SimpleASTNode(ASTNodeType.Programm, "Calculator");
+        SimpleASTNode child = additive(reader);
+        //        if (child != null) {
+        //            node.addChild(child);
+        //        }
+        //        return node;
+        return child;
     }
 
     private static class SimpleASTNode implements ASTNode {
--- src/ch01Craft/section03/SimpleCalculatorTest.java
+++ src/ch01Craft/section03/SimpleCalculatorTest.java
@@ -6,6 +6,8 @@ import ch01Craft.TokenReader;
 import ch01Craft.section02.SimpleLexer;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by JJBOOM on 2020/01/04.
  */
@@ -30,6 +32,36 @@ public class SimpleCalculatorTest {
 
     @org.junit.Test
     public void evaluate() throws Exception {
+        Object[][] scripts = {
+                {"1+2", 3},
+                {"3-4", -1},
+                {"3*4", 12},
+                {"3/4", 0},
+                {"4+2*3", 10},
+                {"8-5/2", 6},
+                {"2*2+4", 8},
+                {"9/2-2", 2},
+                {"3 * (1 +2)", 9},
+                {"1 + 2 + 3", 6},
+                {"(1 + 2) + 3)", 6},
+                {"1 + (2 + 3)", 6},
+                {"2 * 3 * 4", 24},
+                {"(2 * 3) * 4", 24},
+        };
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        for (Object[] pair : scripts) {
+            int result = calculator.evaluate((String) pair[0]);
+            assertEquals(pair[1], result);
+            System.out.println("============================================================================");
+        }
+    }
+
+    @Test
+    public void leftRecursive() throws Exception {
+        String s1 = "1+2+3+4";
+        SimpleCalculator calculator=new SimpleCalculator();
+        calculator.evaluate(s1);
     }
 
 }
